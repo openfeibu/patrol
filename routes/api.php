76147757fe29042470542368c2224e8206e19aa8
wzj <wzj@46de60ef-42c3-9145-b824-3cc8076a95e3>
2019-05-13 06:27:09 +0000

    git-svn-id: https://39.108.144.9:8443/svn/patrol@5 46de60ef-42c3-9145-b824-3cc8076a95e3

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ $api->version('v1', function ($api) {
     $api->get('/order/{id}','App\Http\Controllers\Api\OrderController@getOrder');
     $api->post('/save_order','App\Http\Controllers\Api\OrderController@saveOrder');
     $api->post('/upload_order_image','App\Http\Controllers\Api\OrderController@uploadImage');
-
+    $api->post('/change_password','App\Http\Controllers\Api\UserController@changePassword');
 
     $api->post('register', 'App\Http\Controllers\Api\Auth\RegisterController@register');
     $api->get('/page','App\Http\Controllers\Api\PageController@getPages');
