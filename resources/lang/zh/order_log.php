f595dd28376818040834e6ea0e00edea8410f68e
wzj <wzj@46de60ef-42c3-9145-b824-3cc8076a95e3>
2019-06-03 17:07:43 +0000

    git-svn-id: https://39.108.144.9:8443/svn/patrol@58 46de60ef-42c3-9145-b824-3cc8076a95e3

---
+++ resources/lang/zh/order_log.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'name' => '备注',
+    'label' => [
+
+    ],
+    'admin_type' => [
+        'admin' => '平台管理员',
+        'payment' => '支付商管理员',
+        'provider' => '服务商管理员',
+    ],
+];
